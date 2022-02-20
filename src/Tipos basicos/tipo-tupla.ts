6d443992367046dd6e47e702a36987d121347e3c
claudiodantas <claudiodantas1996@gmail.com>
2022-02-19 23:24:18 -0300

    tipo tupla

--- src/Tipos basicos/tipo-tupla.ts
+++ src/Tipos basicos/tipo-tupla.ts
@@ -2,7 +2,7 @@
 // elements it contains, and exactly which types it contains at specific
 // positions.
 
-const dados: [number, string] = [1, 'claudio'];
+const dados: [number, string] = [1, 'cláudio'];
 const dados2: [number, string, string] = [2, 'vitor', 'natalia'];
 const dados3: [string, number, number?] = ['sandro', 29];
 const dados4: [string, number, ...number[]] = ['claudio', 1, 2, 3, 4, 5, 5];
